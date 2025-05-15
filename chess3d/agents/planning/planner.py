broadcaster fixes

chess3d/agents/planning/planner.py
@@ -105,19 +105,19 @@ class AbstractPlanner(ABC):
                                                              slew_angles))
 
         # check if tasks are clusterable
-        adj : Dict[SchedulableObservationTask, set[SchedulableObservationTask]] = {task : set() for task in schedulable_tasks}
+        adj : Dict[str, set[SchedulableObservationTask]] = {task.id : set() for task in schedulable_tasks}
         
         for i in tqdm(range(len(schedulable_tasks)), leave=False, desc="Checking task clusterability"):
             for j in range(i + 1, len(schedulable_tasks)):
                 if schedulable_tasks[i].can_combine(schedulable_tasks[j]):
-                    adj[schedulable_tasks[i]].add(schedulable_tasks[j])
-                    adj[schedulable_tasks[j]].add(schedulable_tasks[i]) 
+                    adj[schedulable_tasks[i].id].add(schedulable_tasks[j])
+                    adj[schedulable_tasks[j].id].add(schedulable_tasks[i]) 
    
         # sort tasks by degree of adjacency 
         v : list[SchedulableObservationTask] = self.sort_tasks_by_degree(schedulable_tasks, adj)
         
         # only keep tasks that have at least one clusterable task
-        v = [task for task in v if len(adj[task]) > 0]
+        v = [task for task in v if len(adj[task.id]) > 0]
         # print(f'\n\n{len(v)} tasks are clusterable out of {len(tasks)} tasks.')
         
         # combine tasks into clusters
@@ -126,7 +126,7 @@ class AbstractPlanner(ABC):
         with tqdm(total=len(v), desc="Merging overlapping tasks", leave=False) as pbar:
             while len(v) > 0:
                 p : SchedulableObservationTask = v.pop(0)
-                n_p : list[SchedulableObservationTask] = self.sort_tasks_by_degree(list(adj[p]), adj)
+                n_p : list[SchedulableObservationTask] = self.sort_tasks_by_degree(list(adj[p.id]), adj)
 
                 clique : set = {p}
                 pbar.update(1)
@@ -140,24 +140,23 @@ class AbstractPlanner(ABC):
                     clique.add(q)
 
                     # Delete edges from q and p that are not connected to their common neighbors
-                    common_neighbors : set[SchedulableObservationTask] = adj[p].intersection(adj[q])
+                    common_neighbors : set[SchedulableObservationTask] = adj[p.id].intersection(adj[q.id])
 
-                    neighbors_to_delete_p : set[SchedulableObservationTask] = adj[p].difference(common_neighbors)
+                    neighbors_to_delete_p : set[SchedulableObservationTask] = adj[p.id].difference(common_neighbors)
                     for neighbor in neighbors_to_delete_p: 
-                        adj[p].remove(neighbor)
-                        adj[neighbor].remove(p)
+                        adj[p.id].remove(neighbor)
+                        adj[neighbor.id].remove(p)
 
-                    neighbors_to_delete_q : set[SchedulableObservationTask] = adj[q].difference(common_neighbors)
+                    neighbors_to_delete_q : set[SchedulableObservationTask] = adj[q.id].difference(common_neighbors)
                     for neighbor in neighbors_to_delete_q: 
-                        adj[q].remove(neighbor)
-                        adj[neighbor].remove(q)
+                        adj[q.id].remove(neighbor)
+                        adj[neighbor.id].remove(q)
 
                     # Reset neighbor collection N_p for the new p;
-                    n_p : list[SchedulableObservationTask] = self.sort_tasks_by_degree(n_p, adj)
+                    n_p : list[SchedulableObservationTask] = self.sort_tasks_by_degree(list(adj[p.id]), adj)
 
                 for q in clique: 
                     p = p.merge(q)
-                    adj.pop(q)
                     if q in v: 
                         v.remove(q)
                         pbar.update(1)
@@ -175,16 +174,12 @@ class AbstractPlanner(ABC):
 
         # return tasks
         return schedulable_tasks
-        
-    # def get_coordinates(self, ground_points : dict, grid_index : int, gp_index : int) -> list:
-    #     """ Returns the coordinates of the ground points. """
-    #     lat,lon = ground_points[grid_index][gp_index]
-    #     return [lat, lon, 0.0]
-    
+            
+    @runtime_tracker
     def sort_tasks_by_degree(self, tasks : list, adjacency : dict) -> list:
         """ Sorts tasks by degree of adjacency. """
         # calculate degree of each task
-        degrees : dict = {task : len(adjacency[task]) for task in tasks}
+        degrees : dict = {task : len(adjacency[task.id]) for task in tasks}
 
         # sort tasks by degree and return
         return sorted(tasks, key=lambda p: (degrees[p], sum([parent_task.reward for parent_task in p.parent_tasks]), p.accessibility), reverse=True)
@@ -197,50 +192,70 @@ class AbstractPlanner(ABC):
                          orbitdata : OrbitData,
                          observation_history : ObservationHistory
                          ) -> float:
+        dt = []
+        t_0 = time.perf_counter()
+
         # estimate observation look angle
         th_img = np.average((task.slew_angles.left, task.slew_angles.right))
         
         t_l = task.accessibility.left / orbitdata.time_step - 0.5
         t_u = task.accessibility.right / orbitdata.time_step + 0.5
 
+        dt.append(time.perf_counter() - t_0)
+        t_0 = time.perf_counter()
+
         # calculate task performance
-        observation_performances = [row 
-                                    for _,row in orbitdata.gp_access_data.iterrows()
-                                    if row['instrument'] == task.instrument_name
-                                    and any([(row['GP index'] == gp_index and row['grid index'] == grid_index)
-                                                for parent_task in task.parent_tasks
-                                                for *_,grid_index,gp_index in parent_task.targets])
-                                    and t_l <= row['time index'] <= t_u
-                                    and abs(row['look angle [deg]'] - th_img) <= cross_track_fovs[task.instrument_name] / 2
-                                    ]
-        if not observation_performances: return 0.0
+        task_targets = {(int(grid_index), int(gp_index))
+                        for parent_task in task.parent_tasks
+                        for *_,grid_index,gp_index in parent_task.targets}
         
-        instrument_specs = [instr for instr in specs.instrument
-                            if instr.name == task.instrument_name][0].mode[0]
+        columns = [column for column in orbitdata.gp_access_data.columns.values]
+        observation_performances = [(t_index, gp_index, pnt_opt, lat, lon, range, look_angle, *_, grid_index, instrument, agent_name)
+                                    for t_index, gp_index, pnt_opt, lat, lon, range, look_angle, *_, grid_index, instrument, agent_name in orbitdata.gp_access_data.values
+                                    if instrument == task.instrument_name
+                                    and t_l <= t_index <= t_u
+                                    and abs(look_angle - th_img) <= cross_track_fovs[task.instrument_name] / 2
+                                    and (int(grid_index), int(gp_index)) in task_targets
+                                    ]
+        
+        dt.append(time.perf_counter() - t_0)
+        t_0 = time.perf_counter()
+
+        if not observation_performances: 
+            return 0.0
+        
+        instrument_spec : BasicSensorModel = next(instr for instr in specs.instrument).mode[0]
         observation_performance = observation_performances[0]
-        grid_index = int(observation_performance['grid index'])
-        gp_index = int(observation_performance['GP index'])
+        grid_index = observation_performance[len(observation_performance)-3]
+        gp_index = observation_performance[1]
 
         prev_obs = observation_history.get_observation_history(grid_index, gp_index)
 
-        measurement = {
-            "instrument" : task.instrument_name,    
-            "t_start" : task.accessibility.left,
-            "t_end" : task.accessibility.right,
-            "horizontal_spatial_resolution" : observation_performance['ground pixel cross-track resolution [m]'],
-            "accuracy" : instrument_specs.accuracy,
-            "n_obs" : prev_obs.n_obs,
-            "t_prev" : prev_obs.t_last,
-            'spectral_resolution' : instrument_specs.spectral_resolution
-        }
+        dt.append(time.perf_counter() - t_0)
+        t_0 = time.perf_counter()
 
-        # if 'vnir' in task.instrument_name.lower():
-        #     if 'hyp' in task.instrument_name.lower():
-        #         measurement['spectral_resolution'] = "Hyperspectral"
-        #     elif 'multi' in task.instrument_name.lower():
-        #         measurement['spectral_resolution'] = "Multispectral"
-        #     else:
-        #         measurement['spectral_resolution'] = "None"
+        if task.instrument_name.lower() in ['vnir', 'tir']:
+            measurement = {
+                "instrument" : task.instrument_name,    
+                "t_start" : task.accessibility.left,
+                "t_end" : task.accessibility.right,
+                "n_obs" : prev_obs.n_obs,
+                "t_prev" : prev_obs.t_last,
+                "horizontal_spatial_resolution" : observation_performance[columns.index('ground pixel cross-track resolution [m]')],
+                'spectral_resolution' : instrument_spec.spectral_resolution
+            }
+        elif task.instrument_name.lower() == 'altimeter':
+            measurement = {
+                "instrument" : task.instrument_name,    
+                "t_start" : task.accessibility.left,
+                "t_end" : task.accessibility.right,
+                "n_obs" : prev_obs.n_obs,
+                "t_prev" : prev_obs.t_last,
+                "horizontal_spatial_resolution" : observation_performance[columns.index('ground pixel cross-track resolution [m]')],
+                "accuracy" : observation_performance[columns.index('accuracy [m]')],
+            }
+        else:
+            raise NotImplementedError(f'Calculation of task reward not yet supported for instruments of type `{task.instrument_name}`.')
 
         # calculate total task reward          
         task_reward = 0
@@ -252,6 +267,9 @@ class AbstractPlanner(ABC):
             
             u_ijk = task_priority * task_performance * task_score * task_severity
             task_reward += u_ijk
+
+        dt.append(time.perf_counter() - t_0)
+        t_0 = time.perf_counter()
 
         # return total task reward
         return task_reward
@@ -833,7 +851,7 @@ class AbstractPreplanner(AbstractPlanner):
         access_opportunities = {}
         
         for data in tqdm(raw_coverage_data, 
-                         desc='PREPLANNER: Compiling access opportunities', 
+                         desc=f'{state.agent_name}-PREPLANNER: Compiling access opportunities', 
                          leave=False):
             t_img = data[orbitdata_columns.index('time index')]
             grid_index = data[orbitdata_columns.index('grid index')]
@@ -1009,18 +1027,21 @@ class AbstractReplanner(AbstractPlanner):
                         current_plan : Plan,
                         clock_config : ClockConfig,
                         orbitdata : OrbitData,
+                        mission : Mission,
+                        tasks : list,
+                        observation_history : ObservationHistory,
                     ) -> Plan:
         pass
 
     def get_broadcast_contents(self,
                                broadcast_action : FutureBroadcastMessageAction,
                                state : SimulationAgentState,
-                               reward_grid ,
+                               observation_history : ObservationHistory,
                                **kwargs
                                ) -> BroadcastMessageAction:
         """  Generates a broadcast message to be sent to other agents """
         if broadcast_action.broadcast_type == FutureBroadcastTypes.REWARD:
-            raise NotImplementedError('Reward broadcast not yet implemented.')
+            # raise NotImplementedError('Reward broadcast not yet implemented.')
 
             latest_observations : list[ObservationAction] = []
             for grid in reward_grid.rewards:

chess3d/agents/planning/preplanners/dynamic.py
@@ -1,5 +1,6 @@
 from itertools import repeat
 from logging import Logger
+import math
 import os
 from typing import Dict
 from orbitpy.util import Spacecraft
@@ -12,6 +13,7 @@ from dmas.clocks import *
 
 from chess3d.agents.planning.plan import Plan, Preplan
 # from chess3d.agents.planning.rewards import RewardGrid
+from chess3d.agents.planning.tasks import ObservationHistory, SchedulableObservationTask
 from chess3d.agents.states import *
 from chess3d.agents.actions import *
 from chess3d.agents.science.requests import *
@@ -19,6 +21,7 @@ from chess3d.agents.states import SimulationAgentState
 from chess3d.agents.orbitdata import OrbitData
 from chess3d.agents.planning.planner import AbstractPreplanner
 from chess3d.messages import *
+from chess3d.utils import EmptyInterval, Interval
 
 class DynamicProgrammingPlanner(AbstractPreplanner):
     def __init__(self, 
@@ -37,86 +40,137 @@ class DynamicProgrammingPlanner(AbstractPreplanner):
         
     @runtime_tracker
     def _schedule_observations(self, 
-                               state: SimulationAgentState, 
-                               specs: object, 
-                               reward_grid,
-                               _: ClockConfig, 
-                               orbitdata: OrbitData = None
+                               state : SimulationAgentState, 
+                               specs : object, 
+                               _ : ClockConfig, 
+                               orbitdata : OrbitData, 
+                               schedulable_tasks : list,
+                               observation_history : ObservationHistory
                                ) -> list:
         
         if not isinstance(state, SatelliteAgentState):
-            raise NotImplementedError(f'Dynamic Programming planner not yet implemented for agents of type `{type(state)}.`')
+            raise NotImplementedError(f'Naive planner not yet implemented for agents of type `{type(state)}.`')
         elif not isinstance(specs, Spacecraft):
             raise ValueError(f'`specs` needs to be of type `{Spacecraft}` for agents with states of type `{SatelliteAgentState}`')
+        
+        # compile list of instruments available in payload
+        payload : dict = {instrument.name: instrument for instrument in specs.instrument}
+        
+        # compile instrument field of view specifications   
+        cross_track_fovs : dict = self.collect_fov_specs(specs)
+        
+        # get pointing agility specifications
+        adcs_specs : dict = specs.spacecraftBus.components.get('adcs', None)
+        assert adcs_specs, 'ADCS component specifications missing from agent specs object.'
 
-        t_0 = time.perf_counter()
-        t_prev = t_0
+        max_slew_rate = float(adcs_specs['maxRate']) if adcs_specs.get('maxRate', None) is not None else None
+        assert max_slew_rate, 'ADCS `maxRate` specification missing from agent specs object.'
 
-        # compile access times for this planning horizon
-        ground_points : dict = self.get_ground_points(orbitdata)
-        access_opportunities : list  = self.calculate_access_opportunities(state, specs, ground_points, orbitdata)
+        max_torque = float(adcs_specs['maxTorque']) if adcs_specs.get('maxTorque', None) is not None else None
+        assert max_torque, 'ADCS `maxTorque` specification missing from agent specs object.'
 
-        # sort by observation time
-        access_opportunities.sort(key=lambda a: a[3])
+        # sort tasks by start time
+        schedulable_tasks : list[SchedulableObservationTask] = sorted(schedulable_tasks, key=lambda t: (t.accessibility.left, -len(t.parent_tasks)))
 
         # initiate results arrays
-        t_imgs = [np.NAN for _ in access_opportunities]
-        th_imgs = [np.NAN for _ in access_opportunities]
-        rewards = [0.0 for _ in access_opportunities]
-        cumulative_rewards = [0.0 for _ in access_opportunities]
-        preceeding_observations = [np.NAN for _ in access_opportunities]
-        # adjacency = [[False for _ in access_opportunities] for _ in access_opportunities]
+        t_imgs : list[Interval]             = [task.accessibility for task in schedulable_tasks]
+        th_imgs : list[float]               = [np.average((task.slew_angles.left, task.slew_angles.right)) for task in schedulable_tasks]
+        rewards : list[float]               = [self.calc_task_reward(task, specs, cross_track_fovs, orbitdata, observation_history)
+                                                for task in tqdm(schedulable_tasks,leave=False,desc='SATELLITE: Calculating task rewards')]
+        cumulative_rewards : list[float]    = [0.0 for _ in schedulable_tasks]
+        preceeding_observations : list[int] = [np.NAN for _ in schedulable_tasks]
+        observation_actions : list[ObservationAction] = [None for _ in schedulable_tasks]
 
-        t_1 = time.perf_counter() - t_prev
-        t_prev = time.perf_counter()
+        # add dummy values 
+        instrument_names = list(payload.keys())
+        dummy_task = SchedulableObservationTask(set([]), instrument_names[0], Interval(state.t,state.t), Interval(state.attitude[0],state.attitude[0]))
+        schedulable_tasks.insert(0,dummy_task)
+        t_imgs.insert(0,dummy_task.accessibility)
+        th_imgs.insert(0,dummy_task.slew_angles.left)
+        rewards.insert(0,0.0)
+        cumulative_rewards.insert(0,0.0)
+        preceeding_observations.insert(0,np.NAN)
+        observation_actions.insert(0,None)
 
-        # create adjancency matrix      
-        adjacency = [ [access_opportunities.index(prev_opportunity) < access_opportunities.index(curr_opportunity) 
-                            and self.is_observation_path_valid(state, specs, 
-                                                                [ObservationAction( prev_opportunity[2], 
-                                                                                    [*ground_points[prev_opportunity[0]][prev_opportunity[1]],0.0], 
-                                                                                    prev_opportunity[5][0], 
-                                                                                    prev_opportunity[4][0]),
-                                                                ObservationAction( curr_opportunity[2], 
-                                                                                    [*ground_points[curr_opportunity[0]][curr_opportunity[1]],0.0], 
-                                                                                    curr_opportunity[5][-1], 
-                                                                                    curr_opportunity[4][-1])]
-                                                                ) 
-                        
-                        for curr_opportunity in access_opportunities]
-                    for prev_opportunity in tqdm(access_opportunities, 
-                                                desc=f'{state.agent_name}-PLANNER: Generating Adjacency Matrix', 
-                                                leave=False)]
-               
-        t_2 = time.perf_counter() - t_prev
-        t_prev = time.perf_counter()
+        # create adjancency matrix     
+        adjacency = [ [False for _ in schedulable_tasks] for _ in schedulable_tasks]
+
+        for i in tqdm(range(len(schedulable_tasks)), 
+                        desc=f'{state.agent_name}-PLANNER: Generating Adjacency Matrix',
+                        leave=False):
+            # get task i
+            task_i : SchedulableObservationTask = schedulable_tasks[i]
+            
+            # collect all targets and objectives
+            targets_i = [[target[0], target[1], 0.0] 
+                         for parent_task in task_i.parent_tasks
+                         for target in parent_task.targets]
+            objectives = [parent_task.objective for parent_task in task_i.parent_tasks]
+            th_i = th_imgs[i]
+
+            # estimate observation action for task i
+            observation_i = ObservationAction(task_i.instrument_name,
+                                              targets_i,
+                                              objectives,
+                                              th_i,
+                                              task_i.accessibility.left,
+                                              task_i.accessibility.right,
+                                              )
+
+            for j in range(i + 1, len(schedulable_tasks)):
+                # get task j
+                task_j : SchedulableObservationTask = schedulable_tasks[j]
+
+                # collect all targets and objectives
+                targets_j = [[target[0], target[1], 0.0] 
+                            for parent_task in task_j.parent_tasks
+                            for target in parent_task.targets]
+                objectives = [parent_task.objective for parent_task in task_j.parent_tasks]
+                th_j = th_imgs[j]
+
+                # estimate observation action for task j
+                observation_j = ObservationAction(task_j.instrument_name,
+                                                targets_j,
+                                                objectives,
+                                                th_j,
+                                                task_j.accessibility.left,
+                                                task_j.accessibility.right,
+                                                )
+
+                # update observation actions list
+                observation_actions[i] = observation_i
+                observation_actions[j] = observation_j
+
+                # update adjacency matrix
+                adjacency[i][j] = self.is_observation_path_valid(state, specs, [observation_i, observation_j])
+                adjacency[j][i] = self.is_observation_path_valid(state, specs, [observation_j, observation_i])
 
         # calculate optimal path and update results
-        for j in tqdm(range(len(access_opportunities)), 
+        for j in tqdm(range(len(schedulable_tasks)), 
                       desc=f'{state.agent_name}-PLANNER: Calculating Optimal Path',
                       leave=False):
             self.find_optimal_path(state, 
                                    specs, 
-                                   reward_grid, 
-                                   access_opportunities, 
-                                   ground_points, 
+                                   schedulable_tasks, 
                                    adjacency, 
                                    t_imgs, 
                                    th_imgs, 
                                    rewards, 
-                                   cumulative_rewards, preceeding_observations, j)
-            
-        t_3 = time.perf_counter() - t_prev
-        t_prev = time.perf_counter()
+                                   cumulative_rewards, 
+                                   preceeding_observations, 
+                                   observation_actions,
+                                   j
+                                )
+
+        # get task with highest cummulative reward
+        best_task_index = self.argmax(cumulative_rewards)
 
         # extract sequence of observations from results
         visited_observation_opportunities = set()
+        observation_sequence = [best_task_index] if preceeding_observations else []
 
-        while preceeding_observations and np.isnan(preceeding_observations[-1]):
-            preceeding_observations.pop()
-        observation_sequence = [len(preceeding_observations)-1] if preceeding_observations else []
-        
-        while preceeding_observations and not np.isnan(preceeding_observations[observation_sequence[-1]]):
+        while (preceeding_observations 
+               and not np.isnan(preceeding_observations[observation_sequence[-1]])):
             prev_observation_index = preceeding_observations[observation_sequence[-1]]
             
             if prev_observation_index in visited_observation_opportunities:
@@ -125,102 +179,56 @@ class DynamicProgrammingPlanner(AbstractPreplanner):
             visited_observation_opportunities.add(prev_observation_index)
             observation_sequence.append(prev_observation_index)
 
-        observation_sequence.sort()
-
-        t_4 = time.perf_counter() - t_prev
-        t_prev = time.perf_counter()
+        assert 0 in observation_sequence, 'invalid sequence of observations generated by DP. No starting point found.'
+        observation_sequence.reverse()
 
         # create observation actions
-        observations : list[ObservationAction] = []
-        for j in observation_sequence:
-            grid_index, gp_indx, instrument_name, *_ = access_opportunities[j]
-            lat,lon = ground_points[grid_index][gp_indx]
-            target = [lat,lon,0.0]
-            observation = ObservationAction(instrument_name, target, th_imgs[j], t_imgs[j])
-            observations.append(observation)
-                
-        t_5 = time.perf_counter() - t_prev
-        t_f = time.perf_counter() - t_0
+        # scheduled_tasks : list[SchedulableObservationTask] = [schedulable_tasks[j] for j in observation_sequence]
+        # scheduled_tasks.sort(key=lambda o: o.accessibility)      
+        # scheduled_tasks.pop(0) # remove dummy task
+
+        observations : list[ObservationAction] = [observation_actions[j] for j in observation_sequence]
+
+        observations.sort(key=lambda o: o.t_start)      
+        observations.pop(0) # remove dummy observation  
 
         # return observations
-        return observations    
+        return observations  
+
+    def argmax(self, values, rel_tol=1e-9, abs_tol=0.0):
+        max_val = max(values)
+        for i, val in enumerate(values):
+            if math.isclose(val, max_val, rel_tol=rel_tol, abs_tol=abs_tol):
+                return i
 
     @runtime_tracker
     def find_optimal_path(self, 
                           state : SimulationAgentState, 
-                          specs : dict, 
-                          reward_grid ,
-                          access_opportunities : list, 
-                          ground_points : list, 
+                          specs : dict,
+                          schedulable_tasks : list, 
                           adjancency : list, 
                           t_imgs : list, 
                           th_imgs : list,
                           rewards : list,
                           cumulative_rewards : list,
                           preceeding_observations : list,
+                          observation_actions : list,
                           j : int
                           ) -> tuple:
                    
-        # get current observation opportunity
-        curr_opportunity : tuple = access_opportunities[j]
-        lat,lon = ground_points[curr_opportunity[0]][curr_opportunity[1]]
-        curr_target = [lat,lon,0.0]
-
         # get any possibly prior observation            
-        prev_opportunities : list[tuple] = [(i,access_opportunities[i]) for i in range(0,j)
-                                            if adjancency[i][j] and not np.isnan(th_imgs[i]) ]
+        prev_opportunities : list[tuple] = [i
+                                            for i in range(0,j)
+                                            if adjancency[i][j]]
+        # there are no previous possible observations; skip
+        if not prev_opportunities: return
 
-        # calculate all possible observation actions for this observation opportunity
-        possible_observations = [ObservationAction( curr_opportunity[2], 
-                                                    curr_target, 
-                                                    curr_opportunity[5][k], 
-                                                    curr_opportunity[4][k])
-                                        for k in range(len(curr_opportunity[4]))]
-
-        # update observation time and look angle
-        if not prev_opportunities: # there are no previous possible observations
-            # get possible observation action from the current observation opportunity
-            feasible_observation = next((possible_observation for possible_observation in possible_observations
-                                            if self.is_observation_path_valid(state, specs, [possible_observation])),
-                                        None)
-            
-            # check if an observation is possible
-            if feasible_observation is not None:
-                # update imaging time, look angle, and reward
-                t_imgs[j] = feasible_observation.t_start
-                th_imgs[j] = feasible_observation.look_angle
-                rewards[j] = reward_grid.estimate_reward(feasible_observation)
-
-            return
-
-        prev_observations : Dict[int, ObservationAction] = {i : ObservationAction(prev_opportunity[2], 
-                                                                                    [*ground_points[prev_opportunity[0]][prev_opportunity[1]],0.0], 
-                                                                                    th_imgs[i], 
-                                                                                    t_imgs[i])
-                                                            for i,prev_opportunity in prev_opportunities}
-
-        for i,_ in prev_opportunities: # there are previous possible observations
-            # get previous observation
-            prev_observation : ObservationAction = prev_observations[i]
-                            
-            # check if an observation is possible
-            feasible_observation = next((possible_observation for possible_observation in possible_observations
-                                        if self.is_observation_path_valid(state, specs, [prev_observation, possible_observation])),
-                                        None)
-            
-            # update imaging time, look angle, and reward
-            if feasible_observation is not None:
-                # estimate reward for observation
-                reward = reward_grid.estimate_reward(feasible_observation)
-
-                # update results only if cumulative reward is increased
-                if cumulative_rewards[i] + reward > cumulative_rewards[j] and not np.isnan(t_imgs[i]):
-                    t_imgs[j] = feasible_observation.t_start
-                    th_imgs[j] = feasible_observation.look_angle
-                    rewards[j] = reward
-
-                    cumulative_rewards[j] += cumulative_rewards[i] + rewards[j]
-                    preceeding_observations[j] = i
+        # update cummulative reward
+        for i in prev_opportunities:
+            # update cumulative reward
+            if cumulative_rewards[i] + rewards[j] > cumulative_rewards[j]:
+                cumulative_rewards[j] = cumulative_rewards[i] + rewards[j]
+                preceeding_observations[j] = i
 
     @runtime_tracker
     def populate_adjacency_matrix(self, 

chess3d/agents/planning/preplanners/heuristic.py
@@ -41,7 +41,7 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
         cross_track_fovs : dict = self.collect_fov_specs(specs)
         
         # sort tasks by heuristic
-        schedulable_tasks : list[SchedulableObservationTask] = self.sort_tasks_by_heuristic(schedulable_tasks, specs, cross_track_fovs, orbitdata, observation_history)
+        schedulable_tasks : list[SchedulableObservationTask] = self.sort_tasks_by_heuristic(state, schedulable_tasks, specs, cross_track_fovs, orbitdata, observation_history)
 
         # get pointing agility specifications
         adcs_specs : dict = specs.spacecraftBus.components.get('adcs', None)
@@ -66,19 +66,20 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
             
             # calculate task observation angle
             th_img = np.average((task.slew_angles.left, task.slew_angles.right))
+
+            # task_times = [Interval(obs.t_start) for obs in observations]
             
             # check if there is overlap with previous scheduled observation
             potential_overlap = [Interval(observation.t_start,  observation.t_end) 
                                  for observation in observations
                                  if observation.t_start in task.accessibility 
                                   or observation.t_end in task.accessibility]
-            if any([overlap.overlaps(task.accessibility)
-                    for overlap in potential_overlap]):
+            if any([overlap.overlaps(task.accessibility) for overlap in potential_overlap]):
                 continue
 
             # compare with previous scheduled observation
             actions_prev = [observation for observation in observations
-                            if observation.t_end <= task.accessibility.left]
+                            if observation.t_start - 1e-6 <= task.accessibility.left]
             
             if actions_prev:
                 # sort by end time 
@@ -94,9 +95,8 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
                 th_prev = state.attitude[0]
 
             # check if there is a potential previous observation conflict
-            t_img = task.accessibility.left
             prev_action_feasible = self.is_observation_feasible(state,
-                                                                t_img,
+                                                                task.accessibility.left,
                                                                 th_img,
                                                                 t_prev,
                                                                 th_prev,
@@ -107,7 +107,7 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
 
             # compare with future scheduled observation
             actions_next = [observation for observation in observations
-                            if observation.t_start >= task.accessibility.right]
+                            if task.accessibility.right - 1e-6 <= observation.t_end]
             if actions_next:
                 # sort by start times
                 actions_next.sort(key=lambda a : a.t_start)
@@ -122,11 +122,10 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
                 th_next = np.average((task.slew_angles.left, task.slew_angles.right))
 
             # check if there is a potential future observation conflict
-            t_img = task.accessibility.right
             next_action_feasible = self.is_observation_feasible(state,
                                                                 t_next,
                                                                 th_next,
-                                                                t_img,
+                                                                task.accessibility.right,
                                                                 th_img,
                                                                 max_slew_rate,
                                                                 max_torque,
@@ -149,30 +148,55 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
         # sort by start time
         observations_sorted = sorted(observations, key=lambda a : a.t_start)
 
-        assert self.no_redundant_observations(state, observations_sorted, orbitdata)
+        # assert self.no_redundant_observations(state, observations_sorted, orbitdata)
+        assert self.is_observation_path_valid(state, specs, observations_sorted)
 
         return observations_sorted
     
-    def sort_tasks_by_heuristic(self, tasks : list, specs : Spacecraft, cross_track_fovs : dict, orbitdata : OrbitData, observation_history : ObservationHistory) -> list:
-        def reward_heuristic(task : SchedulableObservationTask) -> float:
-            """ Heuristic function to sort tasks by their heuristic value. """
-            # calculate task priority
-            priority = np.prod([parent_task.objective.priority  
-                                for parent_task in task.parent_tasks])
-            # calculate task duration
-            duration = task.accessibility.span()
+    def sort_tasks_by_heuristic(self, state : SimulationAgentState, tasks : list, specs : Spacecraft, cross_track_fovs : dict, orbitdata : OrbitData, observation_history : ObservationHistory) -> list:
+        """ Sorts tasks by heuristic value """
+        if tasks:
+            # estimate maximum number of tasks in the planning horizon
+            min_task_duration = min([task.accessibility.span() for task in tasks])
+            max_number_tasks = int(self.horizon / min_task_duration) if min_task_duration > 0 else -1
             
-            # calculate task start time
-            t_start = task.accessibility.left
-            
-            # calculate task reward
-            task_reward = self.calc_task_reward(task, specs, cross_track_fovs, orbitdata, observation_history)
+            # reduce number of tasks to be scheduled by using estimated max number of tasks 
+            tasks.sort(key=lambda x: x.accessibility.span(),reverse=True)
+            tasks = tasks[:max_number_tasks]
 
-            # return to sort using: highest task reward >> highest priority >> longest duration >> earliest start time
-            return -task_reward, -priority, -duration, t_start
+        # calculate heuristic value for each task
+        values = [[self.calc_heuristic(task, specs, cross_track_fovs, orbitdata, observation_history)] 
+                  for task in tqdm(tasks, desc=f"{state.agent_name}-PREPLANNER: Calculating heuristic values", leave=False)]
+        for i in range(len(values)): values[i].insert(0,tasks[i])
+        
+        # sort tasks by heuristic value
+        sorted_data = sorted(values, key=lambda x: x[1:])
+        
+        # return sorted tasks
+        return [task for task, *_ in sorted_data]
+    
+    def calc_heuristic(self,
+                        task : SchedulableObservationTask, 
+                        specs : Spacecraft, 
+                        cross_track_fovs : dict, 
+                        orbitdata : OrbitData, 
+                        observation_history : ObservationHistory
+                        ) -> float:
+        """ Heuristic function to sort tasks by their heuristic value. """
+        # calculate task priority
+        priority = np.prod([parent_task.objective.priority  
+                            for parent_task in task.parent_tasks])
+        # calculate task duration
+        duration = task.accessibility.span()
+        
+        # calculate task start time
+        t_start = task.accessibility.left
+        
+        # calculate task reward
+        task_reward = self.calc_task_reward(task, specs, cross_track_fovs, orbitdata, observation_history)
 
-        # TEMP SOLUTION schedule based on largest duration and earliest start time
-        return sorted(tasks, key=reward_heuristic)
+        # return to sort using: highest task reward >> highest priority >> longest duration >> earliest start time
+        return -task_reward, -priority, -duration, t_start
 
     def is_observation_feasible(self, 
                                 state : SimulationAgentState,
@@ -196,7 +220,7 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
         dt_maneuver = dth_img / max_slew_rate
 
         # check slew constraint
-        return dt_maneuver <= dt_obs or abs(dt_maneuver - dt_obs) <= 1e-6
+        return dt_obs >= 0 and (dt_maneuver <= dt_obs or abs(dt_maneuver - dt_obs) <= 1e-6)
     
         #TODO check torque constraint
     
@@ -218,8 +242,8 @@ class HeuristicInsertionPlanner(AbstractPreplanner):
                 for target_prev in observation_prev.targets:
                     for target_curr in observation_curr.targets:
                         if (
-                            abs(target_prev[0] - target_prev[0]) <= 1e-3
-                            and abs(target_prev[1] - target_prev[1]) <= 1e-3
+                            abs(target_curr[0] - target_prev[0]) <= 1e-3
+                            and abs(target_curr[1] - target_prev[1]) <= 1e-3
                             and (observation_curr.t_start - observation_prev.t_end) <= orbitdata.time_step):
                             return False
             